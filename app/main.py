99f6ebd28be340c13b08a8a0b9837fa523782bde
kyokukou <erin@arxiv.org>
2026-04-28 08:05:03 -0700

    basic pubsub aggregation

--- app/main.py
+++ app/main.py
@@ -1,5 +1,50 @@
+import logging
+from google.cloud import pubsub_v1
+
+PROJECT_ID = "arxiv-devlopment"
+SUBSCRIPTION_ID = "mod-notification-handler"
+BATCH_SIZE = 300
+
+logging.basicConfig(level=logging.INFO)
+
+
 def main():
-    print("hello worlds")
+    logger = logging.getLogger(__name__)
+
+    #get messages
+    subscriber = pubsub_v1.SubscriberClient()
+    subscription_path = subscriber.subscription_path(PROJECT_ID, SUBSCRIPTION_ID)
+    response = subscriber.pull(
+        request={
+            "subscription": subscription_path,
+            "max_messages": BATCH_SIZE,
+        }
+    )
+    if not response.received_messages:
+        logger.warning("No messages found.")
+        return
+
+    #collect data
+    ack_ids = []
+    timestamps = []
+
+    for msg in response.received_messages:
+        message = msg.message
+        timestamps.append(message.publish_time)
+        ack_ids.append(msg.ack_id)
+
+    logger.info(f"Caught {len(ack_ids)} messages")
+    logger.info(f"Timestamps: {', '.join(timestamps)}")
+
+    #acknowledge all
+    subscriber.acknowledge(
+        request={
+            "subscription": subscription_path,
+            "ack_ids": ack_ids,
+        }
+    )
+
+    logger.info("Messages acknowledged")
 
 
 if __name__ == "__main__":
