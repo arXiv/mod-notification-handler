5101985d3289cd9f074759a15c42f4f79e546913
kyokukou <erin@arxiv.org>
2026-05-11 11:02:45 -0700

    try sending a test email

--- app/config.py
+++ app/config.py
@@ -4,6 +4,13 @@ class Settings(BaseSettings):
     CLASSIC_DB_URI: str
     ENV: str = 'LOCAL'
 
+    #email related
+    SEND_EMAILS: bool = False
+    SMTP_PASSWORD: str 
+    SMTP_HOST: str = "mailh.arxiv.org"
+    SMTP_USER: str = "arxiv"
+    MAIL_FROM: str = "e-prints@arxiv.org"
+
     model_config = SettingsConfigDict(
         env_file=".env",
         case_sensitive=False,
